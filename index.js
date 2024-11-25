7b23c855279dbb27b507761b0739d83dfb877ca7
Adarsh Kandu <kandu72121adarsh.com>
2024-11-25 21:46:43 +0530

    Step-5 define Schema and added a database

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const express =require("express");
 const app=express();
 const {courseRouter}=require("./Routes/course");
 const {userRouter}=require("./Routes/user");
 const{ adminRouter }=require("./Routes/admin");
 
-app.use("/user", userRouter);
-app.use("/course",courseRouter);
-app.use("/admin",adminRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course",courseRouter);
+app.use("/api/v1/admin",adminRouter);
 
 
 
